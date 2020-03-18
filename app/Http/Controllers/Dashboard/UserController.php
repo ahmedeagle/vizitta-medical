747c39096be7ae032e7b549467002cbc314a6815
ahmed emam <ahmed.emam@wisyst.com>
2020-03-18 10:25:38 +0200

    =

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -35,6 +35,7 @@ class UserController extends Controller
 
     public function index()
     {
+        //all user table no laraTables
         $data = [];
         $queryStr = '';
         if (request('queryStr')) {
