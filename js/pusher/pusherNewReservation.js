c6b3b1d5a964c5cb80a27772f40a05f9d298c983
= <=>
2020-03-24 17:50:57 +0200

    =

--- js/pusher/pusherNewReservation.js
+++ js/pusher/pusherNewReservation.js
@@ -7,7 +7,7 @@ var notifications = notificationsWrapper.find('li.scrollable-container');
 // Subscribe to the channel we specified in our Laravel Event
 var channel = pusher.subscribe('new-reservation');
 // Bind a function to a Event (the full Laravel class)
-channel.bind('App\\Events\\newReservation', function (data) {
+channel.bind('App\\Events\\NewReservation', function (data) {
     var existingNotifications = notifications.html();
     var avatar = Math.floor(Math.random() * (71 - 20 + 1)) + 20;
     var newNotificationHtml = `<a href="`+data.path+`"><div class="media"><div class="media-left"><span class="avatar avatar-sm avatar-online rounded-circle"> <img  style="height: 60px;" src="`+data.photo+`" class="img-circle" alt="50x50" style="width: 50px; height: 50px;" alt="avatar"><i></i></span> </div> <div class="media-body"><h6 class="media-heading">` + data.title + `</h6> <p class="notification-text font-small-3 text-muted">` + data.content + `</p><small style="direction: ltr;"><time class="media-meta text-muted" style="direction: ltr;">` + data.date + `</time><br>` + data.time + ` </small></div></div></a>`;
