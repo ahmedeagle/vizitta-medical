a8111aef0a370fa4a214302cb5b19dd4e84a4895
= <=>
2020-03-25 10:54:12 +0200

    =

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -34,8 +34,21 @@
                             <ul class="dropdown-menu dropdown-navbar navbar-pink">
                                 @if(takeLastNotifications(5))
                                     @forelse(takeLastNotifications(5) as $notify)
-                                        <li>
-                                            <a href="#" class="clearfix">
+                                        <li data_notify_id="{{$notify -> id}}">
+                                            <a href="
+                                                        @if($notify -> type == 1 )   {{--new reservation notification--}}
+                                            {{route('admin.reservation.view',$notify -> data_id)}}
+
+                                            @elseif($notify -> type == 2){{--user rate reservation--}}
+                                            {{route('admin.comments')}}
+                                            @elseif($notify -> type == 3) {{--user update reservation Date --}}
+                                            {{route('admin.reservation.view',$notify -> data_id)}}
+                                            @else
+                                               #
+                                            @endif
+
+                                            {{route('admin.reservation.view')}}
+                                                " class="clearfix">
                                                 <img src="{{$notify -> notificationable -> logo}}" class="msg-photo"
                                                      alt="Alex's Avatar">
                                                 <span class="msg-body">
