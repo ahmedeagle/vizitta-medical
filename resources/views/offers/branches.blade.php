b0dbb5a608122123783c7b5df1a5c2636dc32e1f
Mahmoud Elzohairy <mahmoud.hamdy@sahabatech.net>
2020-03-25 16:01:19 +0200

    save

--- resources/views/offers/branches.blade.php
+++ resources/views/offers/branches.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.master')
 
-@section('title', 'الافرع الخاصه باكوبون ')
+@section('title', 'الافرع الخاصه بالعرض ')
 
 @section('content')
 @section('breadcrumbs')
-    {!! Breadcrumbs::render('promoCode-branches') !!}
+    {!! Breadcrumbs::render('offers-branches') !!}
 @stop
 
 <div class="page-content">
     <div class="col-md-12">
         <div class="col-md-9 page-header">
-            <h1><i class="menu-icon fa fa-gift"></i>  </h1>
+            <h1><i class="menu-icon fa fa-gift"></i></h1>
         </div>
     </div>
     <div class="col-md-12">
         <table id="dynamic-table" class="table table-striped table-bordered table-hover no-footer" width="100%">
             <thead>
             <tr>
-                <th>رقم الفرع </th>
-                <th>اسم الفرع </th>
+                <th>رقم الفرع</th>
+                <th>اسم الفرع</th>
                 <th>الحاله</th>
-               
+
             </tr>
             </thead>
         </table>
@@ -30,19 +30,20 @@
 
 @section('scripts')
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#dynamic-table').DataTable({
                 serverSide: true,
                 processing: true,
                 responsive: true,
-                ajax: "{{ route('admin.promoCode.databranch',$promoCodeId) }}",
-                columns: [ 
-                    {name: 'branch_id'},                    
-                    {name: 'branch.name_ar', render: function (data) {
+                ajax: "{{ route('admin.offers.databranch', $offerId) }}",
+                columns: [
+                    {name: 'branch_id'},
+                    {
+                        name: 'branch.name_ar', render: function (data) {
                             return (data === "N/A" ? '' : data);
                         }, orderable: false
                     },
-                    {name: 'status'},                      
+                    {name: 'status'},
                 ],
             });
         });
