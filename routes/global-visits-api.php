e81534e0023e258126e5ac4bb083ccfc543b4b3c
Mahmoud Elzohairy <mahmoud.hamdy@sahabatech.net>
2020-04-07 18:17:26 +0200

    save

--- routes/global-visits-api.php
+++ routes/global-visits-api.php
@@ -1,11 +1,13 @@
 <?php
 
 Route::group(['middleware' => ['CheckPassword', 'ChangeLanguage', 'api']], function () {
+    Route::group(['middleware' => ['CheckUserToken', 'CheckUserStatus']], function () {
 
-    Route::group(['prefix' => 'services'], function () {
-        Route::post('/get-service-available-times', 'GlobalVisitsController@getClinicServiceAvailableTimes');
-        Route::post('/reserve-home-clinic-service', 'GlobalVisitsController@reserveHomeClinicService');
+        Route::group(['prefix' => 'services'], function () {
+            Route::post('/get-service-available-times', 'GlobalVisitsController@getClinicServiceAvailableTimes');
+            Route::post('/reserve-home-clinic-service', 'GlobalVisitsController@reserveHomeClinicService');
+        });
+
     });
-
 });
 
