680fbd129dc8c1994f1d10a43a20f60a8b635b97
= <=>
2020-04-14 18:18:52 +0300

    =

--- app/Traits/ConsultingTrait.php
+++ app/Traits/ConsultingTrait.php
@@ -87,7 +87,7 @@ trait ConsultingTrait
             //  ->format('Y-m-d'))
             ->orderBy('day_date')
             ->orderBy('order')
-            ->select('id', 'doctor_id', 'payment_method_id', 'total_price', 'hours_duration', 'day_date', 'from_time', 'to_time', 'rate', 'rate_comment', 'rate_date')
+            ->select('id', 'doctor_id', 'payment_method_id', 'total_price', 'hours_duration', 'day_date', 'from_time', 'to_time', 'doctor_rate', 'rate_comment', 'rate_date')
             ->paginate(PAGINATION_COUNT);
     }
 }
