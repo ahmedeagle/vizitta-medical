28f9b7dcf46fe19be66fbd8dda16062fa04e47b1
mahmoud.elzoheiry <mahmoud.elzoheiry@wisyst.com>
2020-04-16 15:45:46 +0200

    save

---
+++ routes/doctor-cpanel.php
@@ -0,0 +1,23 @@
+<?php
+
+
+Route::prefix('{locale}/doctor')->middleware(['setAPILocale'])->where(['locale' => '[a-zA-Z]{2}'])->group(function () {
+    Route::post('/login', 'DoctorController@login');
+});
+
+#### Start Authenticated Routes
+Route::group(['middleware' => ['CheckDoctorToken:doctor-api']], function () {
+
+    Route::prefix('{locale}/doctor')->middleware(['setAPILocale'])->where(['locale' => '[a-zA-Z]{2}'])->group(function () {
+
+        // Authentication Routes
+        Route::post('/logout', 'DoctorController@logout');
+        Route::post('/refresh', 'DoctorController@refresh');
+
+
+
+
+    });
+
+});
+
