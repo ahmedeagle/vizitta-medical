602068945d53add0cf5d2ec715605d95429c9611
mahmoud.elzoheiry <mahmoud.elzoheiry@wisyst.com>
2020-04-19 21:10:31 +0200

    save

--- app/Http/Resources/CPanel/ReservationResource.php
+++ app/Http/Resources/CPanel/ReservationResource.php
@@ -13,8 +13,23 @@ class ReservationResource extends ResourceCollection
             return [
                 'id' => $data->id,
                 'reservation_no' => $data->reservation_no,
-
-                'created_at' => $data->created_at->format('Y-m-d'),
+                'day_date' => $data->day_date,
+                'from_time' => $data->from_time,
+                'to_time' => $data->to_time,
+                'provider_id' => $data->provider_id,
+                'approved' => $data->approved,
+                'price' => $data->price,
+                'bill_total' => $data->bill_total,
+                'rejection_reason' => $data->rejection_reason,
+                'for_me' => $data->for_me,
+                'branch_no' => $data->branch_no,
+                'is_reported' => $data->is_reported,
+                'mainprovider' => $data->mainprovider,
+                'admin_value_from_reservation_price_Tax' => $data->admin_value_from_reservation_price_Tax,
+                'reservation_total' => $data->reservation_total,
+                'branch_name' => $data->branch_name,
+                'doctor_name' => app()->getLocale() == 'ar' ? $data->doctor->name_ar : $data->doctor->name_en,
+                'user_name' => $data->user->name,
             ];
         });
 
