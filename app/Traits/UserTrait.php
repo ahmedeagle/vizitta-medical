a3c14a43b5bf774ffcc830511f8643fcffa5a7e7
= <=>
2020-04-19 13:15:20 +0200

    =edit user current reservation

--- app/Traits/UserTrait.php
+++ app/Traits/UserTrait.php
@@ -310,6 +310,7 @@ trait UserTrait
             ->where('user_id', $id)
             //->where('day_date', '>=', Carbon::now()
               //  ->format('Y-m-d'))
+            ->whereNull('offer_id')
             ->orderBy('day_date')
             ->orderBy('order')
             ->paginate(PAGINATION_COUNT);
@@ -332,6 +333,7 @@ trait UserTrait
                 $qu->select('id', DB::raw('name_' . app()->getLocale() . ' as name'));
             }, 'people'])
             ->where('user_id', $id)
+            ->whereNull('offer_id')
             ->orderBy('day_date')
             ->orderBy('order')
             ->paginate(PAGINATION_COUNT);
