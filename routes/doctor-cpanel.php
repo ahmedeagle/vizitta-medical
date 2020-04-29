844c2fb80a8e90e78f3099ca17643f420415e234
mahmoud.elzoheiry <mahmoud.elzoheiry@wisyst.com>
2020-04-30 01:39:37 +0200

    save

--- routes/doctor-cpanel.php
+++ routes/doctor-cpanel.php
@@ -14,8 +14,10 @@ Route::group(['middleware' => ['CheckDoctorToken:doctor-api']], function () {
         Route::post('/logout', 'DoctorController@logout');
         Route::post('/refresh', 'DoctorController@refresh');
 
-        Route::group(["namespace" => "DoctorArea"], function () {
+        Route::group(["namespace" => "DoctorArea", "prefix" => "doctor-area"], function () {
             Route::post('/reservations/index', 'DoctorReservationsController@index');
+            Route::post('/reservations/get-rejected-reasons', 'DoctorReservationsController@getRejectedReasons');
+            Route::post('/change-status', 'DoctorReservationsController@changeStatus');
 
         });
     });
