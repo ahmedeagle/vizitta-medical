693a220ba6478823947f500cff1ddc82736a57d7
= <=>
2020-05-06 23:28:54 +0300

    =

--- app/Traits/SearchTrait.php
+++ app/Traits/SearchTrait.php
@@ -36,7 +36,7 @@ trait SearchTrait
             ->where('providers.status', true)
             //->where('test',0)   // not for test porpose
                 ->whereDoesntHave('test',function($t) use ($userId){
-                    $t->where('user_id',$userId);
+                    $t->where('user_id','!=',$userId);
             })
             ->whereNotNull('providers.provider_id');
 
