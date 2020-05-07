4211c5c3e9d483c60428f28aa8079d723108dbe5
mahmoud.elzoheiry <mahmoud.elzoheiry@wisyst.com>
2020-05-07 16:20:48 +0200

    save

--- app/Http/Resources/CPanel/ReportsResource.php
+++ app/Http/Resources/CPanel/ReportsResource.php
@@ -3,19 +3,31 @@
 namespace App\Http\Resources\CPanel;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ReportsResource extends JsonResource
+class ReportsResource extends ResourceCollection
 {
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'reservation_no' => $this->reservation_no,
-            'reporting_type' => app()->getLocale() == 'ar' ? $this->reportingType->name_ar : $this->reportingType->name_en,
-            'user_name' => isset($this->user) ? $this->user->name : null,
-            'provider_name' => isset($this->provider) ? (app()->getLocale() == 'ar' ? $this->provider->name_ar : $this->provider->name_en) : null,
-            'created_at' => $this->created_at,
+        $result['data'] = $this->collection->transform(function ($data) {
+            return [
+                'id' => $data->id,
+                'reservation_no' => $data->reservation_no,
+                'reporting_type' => $data->reporting_type,
+                'user_name' => isset($data->user) ? $data->user->name : null,
+                'provider_name' => isset($data->provider) ? (app()->getLocale() == 'ar' ? $data->provider->name_ar : $data->provider->name_en) : null,
+                'created_at' => $data->created_at->format('Y-m-d'),
+            ];
+        });
+
+        $result['pagination'] = [
+            'total' => $this->total(),
+            'count' => $this->count(),
+            'per_page' => $this->perPage(),
+            'current_page' => $this->currentPage(),
+            'total_pages' => $this->lastPage()
         ];
+        return $result;
     }
 
 }
