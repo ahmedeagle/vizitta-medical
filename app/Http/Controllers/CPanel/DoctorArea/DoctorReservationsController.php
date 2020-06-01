20df3e0b4bff3b9e62fac58d83301a6a76c2810b
= <=>
2020-06-01 10:06:30 +0300

    =admin init chat

--- app/Http/Controllers/CPanel/DoctorArea/DoctorReservationsController.php
+++ app/Http/Controllers/CPanel/DoctorArea/DoctorReservationsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\CPanel\DoctorArea;
 
 use App\Http\Controllers\ChattingController;
-use App\Models\Chat;
 use App\Models\DoctorConsultingReservation;
 use App\Models\Provider;
 use App\Models\Reason;
@@ -220,38 +219,5 @@ class DoctorReservationsController extends Controller
         }
 
         return true;
-    }
-
-
-    public function startChatting($consultReservationId, $userId, $actor_type)
-    {
-        try {
-            ##############check if chat is exist and start before###############
-            $chatId = 0;
-            $checkIfChatExists = Chat::where('consulting_id', $consultReservationId)
-                ->where('chatable_id', $userId)
-                ->where('solved', 0)
-                ->first();
-            #############if not exist store it###############################
-            if (!$checkIfChatExists) {
-                $chat = Chat::create([
-                    'title' => '',
-                    'chatable_id' => $userId,
-                    'chatable_type' => ($actor_type == 1) ? 'App\Models\User' : 'App\Models\Doctor',
-                    'message_no' => $this->getRandomUniqueNumberChatting(8),
-                    'consulting_id' => $consultReservationId,
-                ]);
-                $chatId = $chat->id;
-            } else {
-                $chatId = $checkIfChatExists->id;
-            }
-            $consultReservation = DoctorConsultingReservation::find($consultReservationId);
-            if($consultReservation){
-                $consultReservation -> update(['chatId' => $chatId]);
-            }
-
-        } catch (\Exception $ex) {
-            //return $this->returnError($ex->getCode(), $ex->getMessage());
-        }
     }
 }
