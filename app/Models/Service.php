4d9d779a504fb70115a0c7ff9997ebe4419c2189
= <=>
2020-06-03 00:13:33 +0300

    =

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -12,7 +12,7 @@ class Service extends Model
     protected $table = 'services';
     public $timestamps = true;
 //
-    protected $fillable = ['title_ar', 'title_en', 'information_ar', 'information_en', 'provider_id', 'branch_id', 'specification_id', 'clinic_price', 'home_price', 'clinic_price_duration', 'home_price_duration', 'status', 'type', 'rate', 'reservation_period', 'created_at', 'updated_at'];
+    protected $fillable = ['title_ar', 'title_en', 'information_ar','price', 'information_en', 'provider_id', 'branch_id', 'specification_id', 'clinic_price', 'home_price', 'clinic_price_duration', 'home_price_duration', 'status', 'type', 'rate', 'reservation_period', 'created_at', 'updated_at'];
     protected $hidden = ['specification_id', 'status', 'created_at', 'updated_at'];
 
     protected $forcedNullStrings = ['reservation_period', 'home_price_duration', 'clinic_price_duration'];
@@ -20,7 +20,7 @@ class Service extends Model
     protected $casts = [
         'status' => 'integer',
     ];
-    protected $appends = ['available_time', 'hide','ahmed'];
+    protected $appends = ['available_time', 'hide'];
 
 
     public function getHideAttribute()
@@ -142,9 +142,8 @@ class Service extends Model
 
     //we change price to to other price in all project and not need to edit the key in all places
 
-    public function getahmedAttribute()
+    public function getPriceAttribute()
     {
-         return  $this-> clinic_price . $this->home_price;
         if ($this->clinic_price != null && $this->home_price != null) {
             return ($this->clinic_price <= $this->home_price) ? (string)$this->clinic_price : (string)$this->home_price;
         } elseif ($this->home_price != null) {
