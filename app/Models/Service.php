bff0bece91e69819b74af5927a6aaf339b47c318
= <=>
2020-06-02 16:07:07 +0300

    =favourite consulting

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -144,6 +144,7 @@ class Service extends Model
 
     public function getPriceAttribute($val)
     {
+        return '5002';
         if ($this->clinic_price != null && $this->home_price != null) {
             return $this->clinic_price <= $this->home_price ? (string)$this->clinic_price : (string)$this->home_price;
         } elseif ($this->clinic_price != null) {
