5300b8f9bd46fd9db549c616d6c73742e405e55a
= <=>
2020-06-17 15:41:59 +0300

    =

--- app/Http/Controllers/ConsultingController.php
+++ app/Http/Controllers/ConsultingController.php
@@ -92,9 +92,11 @@ class ConsultingController extends Controller
             $doctorsJson->per_page = PAGINATION_COUNT;
             $doctorsJson->total_count = $total_count;
             $doctorsJson->data = $doctors->data;
-            return $this->returnData('doctors', $doctorsJson);
+
         }
-        return $this->returnError('E001', trans('messages.there is no data found'));
+
+        return $this->returnData('doctors', $doctorsJson);
+ //       return $this->returnError('E001', trans('messages.there is no data found'));
     }
 
     public function getCurrentConsultingReserves(Request $request)
