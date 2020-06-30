cae55b1fc9f3f2b8d015cfe8708a1ae20361d122
= <=>
2020-06-30 13:31:07 +0300

    =

--- app/Http/Controllers/OffersController.php
+++ app/Http/Controllers/OffersController.php
@@ -1824,7 +1824,7 @@ class OffersController extends Controller
                     $comment = " نسبة ميدكال كول من كشف (عرض) حجز نقدي ";
                     $invoice_type = 0;
                     try {
-                     return    $this->calculateOfferReservationBalance($application_percentage_of_offer, $reservation);
+                         $this->calculateOfferReservationBalance($application_percentage_of_offer, $reservation);
                     } catch (\Exception $ex) {
                     }
                 }
@@ -1889,7 +1889,6 @@ class OffersController extends Controller
 
             $provider = $reservation->provider;  // always get branch
 
-            return $reservationBalance;
             $provider->update([
                 'balance' => $provider->balance - $reservationBalance,
             ]);
