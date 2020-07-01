f67ce6bcf32027cfc99444d9963e9da54bb8c239
= <=>
2020-07-01 17:32:58 +0300

    =balance

--- app/Http/Controllers/CPanel/DoctorArea/BalanceController.php
+++ app/Http/Controllers/CPanel/DoctorArea/BalanceController.php
@@ -29,7 +29,7 @@ class BalanceController extends Controller
     {
         try {
               $doctor = $this->getAuthDoctor();
-             return $reservations = $this->getReservationBalanceForConsultingDoctors($doctor -> id);  // get consulting reservation balance of completed reservation
+               $reservations = $this->getReservationBalanceForConsultingDoctors($doctor -> id);  // get consulting reservation balance of completed reservation
             if (count($reservations->toArray()) > 0) {
                 $reservations->getCollection()->each(function ($reservation) use ($request) {
                     $reservation->makeHidden(['order', 'reservation_total', 'admin_value_from_reservation_price_Tax', 'mainprovider', 'is_reported', 'branch_no', 'for_me', 'rejected_reason_id', 'is_visit_doctor', 'rejection_reason', 'user_rejection_reason']);
@@ -58,8 +58,7 @@ class BalanceController extends Controller
 
     private function getReservationBalanceForConsultingDoctors($doctorId)
     {
-        return $doctorId;
-        return $reservations = DoctorConsultingReservation::with(['paymentMethod' => function ($qu) {
+         return $reservations = DoctorConsultingReservation::with(['paymentMethod' => function ($qu) {
             $qu->select('id', DB::raw('name_' . app()->getLocale() . ' as name'));
         }])
             ->whereNull('provider_id')
