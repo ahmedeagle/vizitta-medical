7ecfca6b6fe123cdc1f43ab33ffdcbb3c854ff77
= <=>
2020-07-02 13:54:46 +0300

    =

--- app/Http/Controllers/GlobalConsultingController.php
+++ app/Http/Controllers/GlobalConsultingController.php
@@ -263,7 +263,7 @@ class GlobalConsultingController extends Controller
                 //send sms to consulting doctor
                 $doctorMessage =   'هناك حجز استشارة جديد برقم ' . ' ' . $reservation->reservation_no . ' ' . ' ( ' . $doctorName . ' )';
                 if (!is_null($doctor->phone))
-                    $this->sendSMS($doctor->phone, $smsMessage);  //sms for main provider
+                    $this->sendSMS($doctor->phone, $doctorMessage);  //sms for main provider
 
                 $res = DoctorConsultingReservation::with(['doctor', 'provider', 'branch', 'paymentMethod'])->find($reservation->id);
                 $result = new SingleDoctorConsultingReservationResource($res);
