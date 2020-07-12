83181a031bddec0c3134085f9b1e8478e3d63d42
ahmed emam <ahmed.emam@wisyst.com>
2020-07-12 14:47:05 +0200

    =

--- app/Http/Resources/CPanel/OfferCategoryResource.php
+++ app/Http/Resources/CPanel/OfferCategoryResource.php
@@ -15,6 +15,7 @@ class OfferCategoryResource extends ResourceCollection
                 'name' => app()->getLocale() == 'ar' ? $data->name_ar : $data->name_en,
                 'name_ar' => $data->name_ar,
                 'name_en' => $data->name_en,
+                'status'   =>$data->status,
                 'parentCategory' => $data->parentCategory ? (app()->getLocale() == 'ar' ? $data->parentCategory->name_ar : $data->parentCategory->name_en) : null,
                 'hasTimer' => [
                     'name' => $data->hastimer == 1 ? __('main.active') : __('main.not_active'),
