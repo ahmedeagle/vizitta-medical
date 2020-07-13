1240674f1865024f5a3c9f4c250898df30e1e135
ahmed emam <ahmed.emam@wisyst.com>
2020-07-13 11:42:39 +0200

    =reason

--- app/Http/Controllers/CPanel/DoctorController.php
+++ app/Http/Controllers/CPanel/DoctorController.php
@@ -105,7 +105,7 @@ class DoctorController extends Controller
 
             $res = ['status' => true, 'data' => []];
            //
-             $res['data'] = $doctor;
+             $res['data']['doctor'] = $doctor;
             $res['data']['provider'] = Doctor::find($request->id)->provider()->select('id', 'name_'.app() -> getLocale() .' as name');
             return response()->json($res);
         } catch (\Exception $ex) {
