f4f9c2250805b242d519e07937c74cb4deb5c706
ahmed emam <ahmed.emam@wisyst.com>
2020-07-16 16:27:57 +0200

    =reject services reservation

--- app/Http/Resources/ServiceReservationDetailsResource.php
+++ app/Http/Resources/ServiceReservationDetailsResource.php
@@ -15,7 +15,7 @@ class ServiceReservationDetailsResource extends JsonResource
             'from_time' => $this->from_time,
             'to_time' => $this->to_time,
             'approved' => $this->approved,
-            'price' => $this-> services  -> price,
+            'price' => $this-> service  -> price,
             'total_price' => $this->total_price,
             'bill_total' => $this->bill_total,
             'service_type' => $this->service_type,
